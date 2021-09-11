3f6a19dc9e89fe6df614f3a5d7e76718440530ac
kondim23 <kondim23@gmail.com>
2021-09-11 17:19:08 +0300

    front-end:
    startup - connections

--- src/pages/Network.js
+++ src/pages/Network.js
@@ -1,6 +1,135 @@
+import {Button, Card, Container, FormControl, InputGroup, Row} from "react-bootstrap";
+import {useRef, useState} from "react";
+
 function Network(){
 
-    return <div>Network</div>
+    const searchDataRef = useRef();
+    const [usersPreview, setUsersPreview] = useState([]);
+
+    function searchSubmit(event){
+
+        event.preventDefault();
+
+        fetch("http://localhost:8080/search/"+searchDataRef.current.value,{
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then((response) => {
+
+            return response.json();
+        }).then((users) => {
+
+            setUsersPreview(users);
+        })
+    }
+
+    return (
+
+        <Container>
+            <Row>
+                <InputGroup className="mb-3" onSubmit={searchSubmit}>
+                    <FormControl
+                        placeholder="Enter a full name"
+                        aria-label="Search user"
+                        aria-describedby="basic-addon1"
+                        ref={searchDataRef}
+                    />
+                    <Button type={'submit'}>Search</Button>
+                </InputGroup>
+            </Row>
+            <Row>
+            <Container>
+                <div className={'row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'}>
+                    <div className={'col'}>
+                        <div className={'card shadow-sm'}>
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
+                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"/>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                            <div className="card-body">
+                                <p className="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small className="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={'col'}>
+                        <div className={'card shadow-sm'}>
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
+                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"/>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                            <div className="card-body">
+                                <p className="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small className="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={'col'}>
+                        <div className={'card shadow-sm'}>
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
+                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"/>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                            <div className="card-body">
+                                <p className="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small className="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={'col'}>
+                        <div className={'card shadow-sm'}>
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225"
+                                 xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                                 preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"/>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                            <div className="card-body">
+                                <p className="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small className="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+            </Row>
+        </Container>
+    )
 }
 
 export default Network;
